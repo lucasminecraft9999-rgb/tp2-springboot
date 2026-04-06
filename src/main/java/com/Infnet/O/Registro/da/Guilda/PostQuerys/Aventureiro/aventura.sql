47fc7772e1ca039812e8849b856ee58fe67d9b80
LucasSi7va <lucassilvadesouza9999@gmail.com>
2026-04-06 10:15:23 -0300

    tp2-feito-finalmento

---
+++ src/main/java/com/Infnet/O/Registro/da/Guilda/PostQuerys/Aventureiro/aventura.sql
@@ -0,0 +1,5 @@
+select column_name , data_type from information_schema.columns where table_name = 'aventureiro';
+select column_name , data_type from information_schema.columns where table_name = 'companheiro';
+select column_name , data_type from information_schema.columns where table_name = 'missao';
+select column_name , data_type from information_schema.columns where table_name = 'participacao_em_missao';;
+
